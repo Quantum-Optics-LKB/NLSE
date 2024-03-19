1fc9c1bd07a80896498041279e7ccab2c9d0cf9d
Tangui Aladjidi <taladjidi@gmail.com>
2024-03-19 14:50:37 +0100

    Bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="NLSE",
-    version="1.0.0",
+    version="2.0.0",
     description="A package for solving the Nonlinear Schrödinger Equation"
     " (NLSE) using the Split-Step Fourier method.",
     url="https://github.com/Quantum-Optics-LKB/NLSE",

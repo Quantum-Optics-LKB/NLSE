3998f7e13c73b3ae350b12144728fd9be12c868a
Tangui Aladjidi <taladjidi@gmail.com>
2024-05-30 10:30:46 +0200

    OpenCL installation instructions

--- NLSE/utils.py
+++ NLSE/utils.py
@@ -13,6 +13,10 @@ except ImportError:
 
 
 try:
+    # for OpenCL backend you need to install OpenCL first
+    # sudo apt install intel-opencl-icd opencl-headers ocl-icd-opencl-dev
+    # or for AMD
+    # sudo apt install opencl-headers ocl-icd-opencl-dev
     import pyopencl
 
     __PYOPENCL_AVAILABLE__ = True

ccb4ebade8e37cd0ecb2ca407eeda672eafb2c26
Tangui Aladjidi <taladjidi@gmail.com>
2024-07-29 10:01:45 +0200

    license

--- NLSE/__init__.py
+++ NLSE/__init__.py
@@ -7,7 +7,7 @@ Split-Step Fourier method.
 
 __version__ = "2.3.0"
 __author__ = "Tangui Aladjidi"
-__license__ = "MIT"
+__license__ = "GPLv3"
 __credits__ = "Laboratoire Kastler Brossel, Paris, France"
 __email__ = "tangui.aladjidi@lkb.upmc.fr"
 
